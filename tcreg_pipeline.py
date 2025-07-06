567a33989a8b9632b6ce2e87c017a7545a8b2d72
Sagar Gupta <sagarg@sas.upenn.edu>
2025-07-06 18:31:06 -0400

    Added comments and cleaned up code

--- tcreg_pipeline.py
+++ tcreg_pipeline.py
@@ -3,20 +3,22 @@
 #   Date: May 27, 2024
 #   Email: sagarg@sas.upenn.edu
 
-# import required libraries
+# Import required libraries
 import csv
 from collections import defaultdict
 import argparse
-import os
 from pymol import cmd, stored
+from pathlib import Path
 
 from Bio.Align import substitution_matrices
 blosum62_matrix = substitution_matrices.load("BLOSUM62")
 pos_charge = ['R', 'H', 'K']
 neg_charge = ['D', 'E', 'Q', 'N', 'S', 'T']
 
-# import custom libraries
+# Import custom libraries
 from fasta import FASTA
+
+CUTOFF_DISTANCE = 5  # Angstrom cutoff to define a "contact"
 
 def parse_args():
 
@@ -26,7 +28,7 @@ def parse_args():
 
     parser.add_argument("-tcreg_file", help="Starting T-CREG file. Default is the 21 T-CREG, >5 percent cutoff version. Can be switched to tcregs_greedy_no_cutoff.txt", default="tcregs_greedy_5_percent_cutoff.txt")
 
-    # if you have a structure, you can use it to get more specific TCR contact residues
+    # If you have a structure, you can use it to get more specific TCR contact residues
     parser.add_argument("-structure", help="File containing pHLA-receptor structure", type=str, default="")
     parser.add_argument("-hla_chain", help="Chain name of HLA in the pHLA-receptor structure", type=str, default="A")
     parser.add_argument("-receptor_chain", nargs="+", help="Chain name(s) of the receptor in the pHLA-receptor structure. If you have multiple chain names, give them as -receptor_chain D E F", default=["D", "E"])
@@ -34,15 +36,13 @@ def parse_args():
 
 def get_mutation_list(seq1, seq2):
 
-        seq1_mut, seq2_mut = [], []
-
-        for index, seq1_aa in enumerate(list(seq1)):
-            seq2_aa = list(seq2)[index]
-            if seq1_aa != seq2_aa:
-                seq1_mut.append(seq1_aa)
-                seq2_mut.append(seq2_aa)
-        
-        return seq1_mut, seq2_mut
+    seq1_mut, seq2_mut = [], []
+    for seq1_aa, seq2_aa in zip(seq1, seq2):
+        if seq1_aa != seq2_aa:
+            seq1_mut.append(seq1_aa)
+            seq2_mut.append(seq2_aa)
+    
+    return seq1_mut, seq2_mut
 
 def find_tcregs(seq_tcr_contact, new_alleles):
         
@@ -55,22 +55,30 @@ def find_tcregs(seq_tcr_contact, new_alleles):
             
             seq2 = seq_tcr_contact[allele2]
             
+            # If sequences are identical, group them together
             if seq1 == seq2: 
                 seq_max[allele1].append(allele2)
                 if allele1 not in seq_max[allele1]:
                     seq_max[allele1].append(allele1)
                 continue
-                
+            
+            # Check if mutations between sequences are significant
             seq1_mut, seq2_mut = get_mutation_list(seq1, seq2)
             significant_mut = False
-            for index, aa in enumerate(seq1_mut):
-                seq2_aa = seq2_mut[index]
+            
+            for aa1, aa2 in zip(seq1_mut, seq2_mut):
                 try:
-                    blosum_score = blosum62_matrix[(aa, seq2_aa)]
+                    blosum_score = blosum62_matrix[(aa1, aa2)]
                 except:
-                    blosum_score = blosum62_matrix[(seq2_aa, aa)]
+                    blosum_score = blosum62_matrix[(aa2, aa1)]
 
-                if blosum_score < 0 or (aa in pos_charge and seq2_aa in neg_charge) or (aa in neg_charge and seq2_aa in pos_charge):
+                # Check for charge switches (see Table S1)
+                charge_switch = (
+                    (aa1 in pos_charge and aa2 in neg_charge) or 
+                    (aa1 in neg_charge and aa2 in pos_charge)
+                )
+
+                if blosum_score < 0 or charge_switch:
                     significant_mut = True
                     break
         
@@ -95,13 +103,11 @@ def write_tcreg_file(filename, tcreg, usa_allelic_freq, tcr_contact_resis, seq_t
                 seq2 = sequences[allele2]
                 if seq1 == seq2 or allele1 == allele2: continue
 
-                for index, seq1_aa in enumerate(list(seq1)):
-                    seq2_aa = list(seq2)[index]
-                    hla_pos = tcr_contact_resis[index]
+                for seq1_aa, seq2_aa, hla_pos in zip(seq1, seq2, tcr_contact_resis):
                     if seq1_aa != seq2_aa:
                         allele_combo = sorted([allele1, allele2])
-                        wt_aa = list(sequences[allele_combo[0]])[index]
-                        mut_aa = list(sequences[allele_combo[1]])[index]
+                        wt_aa = sequences[allele_combo[0]][tcr_contact_resis.index(hla_pos)]
+                        mut_aa = sequences[allele_combo[1]][tcr_contact_resis.index(hla_pos)]
                         mutations["_".join(allele_combo)].add(f"{wt_aa}{hla_pos}{mut_aa}")
                         
         return mutations
@@ -113,6 +119,7 @@ def write_tcreg_file(filename, tcreg, usa_allelic_freq, tcr_contact_resis, seq_t
         for allele in allele_list:
             total_allelic_coverage += usa_allelic_freq[allele]
 
+        # Get unique mutations within this T-CREG
         mutations = []
         all_mutations_dict = get_mutations_dict(seq_tcr_contact, allele_list)
         for allele_pair, mut_set in all_mutations_dict.items():
@@ -121,14 +128,14 @@ def write_tcreg_file(filename, tcreg, usa_allelic_freq, tcr_contact_resis, seq_t
                 pos = mut[1:-1]
                 aa1 = mut[0]
                 aa2 = mut[-1]
-                if mut in mutations or f"{aa2}{pos}{aa1}" in mutations: continue
+                if mut in mutations or f"{aa2}{pos}{aa1}" in mutations: 
+                    continue
                 else:
                     mutations.append(mut)
         
-        print(common_allele + " | " + str(round(total_allelic_coverage*100,2))+"%" + " | " + ", ".join(mutations), file=final_file)
+        print(f"{common_allele} | {total_allelic_coverage*100:.2f}% | {', '.join(mutations)}", file=final_file)
         for allele in allele_list:
             print(f" - {allele}", file=final_file)
-
 
 def get_tcreg(hla_file, output_dir, tcreg_file, structure, hla_chain, receptor_chain):
 
@@ -139,12 +146,12 @@ def get_tcreg(hla_file, output_dir, tcreg_file, structure, hla_chain, receptor_c
     Creates sequence logos of non-conserved TCR contact residues for strong binders
     '''
 
-    # Consider HLA (-A, -B, -C) alleles that have an allelic frequency of 0.05% or more in at least one of the 5 major American sub-populations (n = 215)
+    # Consider HLA (-A, -B, -C) alleles that have an allelic frequency of ≥0.05% in at least one of the 5 major American sub-populations (n = 215)
     file = FASTA("imgt_allelic_freq_0.05_NMDP.fasta")  # IPD-IMGT/HLA Release 3.48.0
     file.read()
     hla_seq = file.get_sequences()
 
-    # Get population and allelic frequency values for each allele from the NMDP/BeTheMatch database
+    # Load population and allelic frequency values for each allele from the NMDP/BeTheMatch database
     usa_allelic_freq = defaultdict(float)
     with open("NMDP_per_allele.csv", "r") as csvfile:
         reader = csv.reader(csvfile)
@@ -152,23 +159,23 @@ def get_tcreg(hla_file, output_dir, tcreg_file, structure, hla_chain, receptor_c
         for line in reader:
             usa_allelic_freq[line[0]] = float(line[-2])
 
-    os.system(f"mkdir -p {output_dir}")
-
-    cutoff = 5 # angstrom cutoff to define a "contact"
+    Path(output_dir).mkdir(parents=True, exist_ok=True)
 
     if structure == "":
-
+        # Use pre-computed TCR contact residues from TCR3d database
         tcr_contact_resis = []
-        with open(f"tcr_contact_resi_{cutoff}.csv", "r") as csvfile:
+        with open(f"tcr_contact_resi_{CUTOFF_DISTANCE}.csv", "r") as csvfile:
             reader = csv.reader(csvfile)
             next(reader)
             for line in reader:
-                if float(line[-1]) > 10: # need to have been defined as a contact in at least 10% of pHLA-TCR structures
+                # Only include residues that are contacts in >10% of structures
+                if float(line[-1]) > 10:
                     tcr_contact_resis.append(int(line[1]))
         tcr_contact_resis = sorted(tcr_contact_resis)
         
-        print(f"Evaluating T-CREGs using {len(tcr_contact_resis)} TCR contact residues ({cutoff} A cutoff) based off of structures in TCR3d")
+        print(f"Evaluating T-CREGs using {len(tcr_contact_resis)} TCR contact residues ({CUTOFF_DISTANCE} A cutoff) based on structures in TCR3d")
 
+        # Load existing T-CREG definitions from file
         tcreg = defaultdict(list)
         with open(tcreg_file, "r") as txtfile:
             lines = txtfile.readlines()
@@ -182,25 +189,27 @@ def get_tcreg(hla_file, output_dir, tcreg_file, structure, hla_chain, receptor_c
                     tcreg[current_tcreg] = []
 
     else:
-
-        if not os.path.isfile(structure):
-            print(f"{structure} could not be found! Was the correct path to the structure given?")
-            quit()
+        # Use provided structure to determine TCR contact residues
+        if not Path(structure).is_file():
+            raise FileNotFoundError(f"{structure} could not be found! Was the correct path to the structure given?")
         
         receptor_chain_str = " or ".join([f"chain {chain}" for chain in receptor_chain])
         
         stored.tcr_contact_resis = []
         cmd.load(structure, "struc")
-        cmd.select("interface", f"(br. chain {hla_chain} within {cutoff} of ({receptor_chain_str})) and (chain {hla_chain} and (sc. or name ca))")
+        cmd.remove("hydro and struc") # remove hydrogen atoms
+        cmd.select("interface", f"(br. chain {hla_chain} within {CUTOFF_DISTANCE} of ({receptor_chain_str})) and (chain {hla_chain} and (sc. or name ca))")
         cmd.iterate("interface and name ca", "stored.tcr_contact_resis.append(resi)")
 
         if len(stored.tcr_contact_resis) == 0:
-            print(f"Could not find any TCR contact residues in {structure} - are the chain names correct?")
-            quit()
+            raise ValueError(f"Could not find any TCR contact residues in {structure}. Are the chain names correct?")
 
         tcr_contact_resis = sorted([int(pos) for pos in stored.tcr_contact_resis])
-        print(f"Generating T-CREGs using {len(tcr_contact_resis)} TCR contact residues ({cutoff} A cutoff) based off of {structure}")
+        print(f"Generating T-CREGs using {len(tcr_contact_resis)} TCR contact residues ({CUTOFF_DISTANCE} A cutoff) based off of {structure}")
+        cmd.delete("all")
+        cmd.quit()
         
+        # Extract TCR contact residue sequences for each allele
         seq_tcr_contact = defaultdict(str)
         for allele, seq in hla_seq.items():
             str_tcr_contact = ''
@@ -214,8 +223,8 @@ def get_tcreg(hla_file, output_dir, tcreg_file, structure, hla_chain, receptor_c
         seq_max = find_tcregs(seq_tcr_contact, list(seq_tcr_contact.keys()))
         new_alleles = list(seq_tcr_contact.keys())
 
+        # Greedy algorithm to determine T-CREGss
         temp_tcreg = {}
-        # this is the loop that gets the T-CREGs in a "greedy" manner i.e., it is looking for HLAs are similar to the most other HLAs
         while True:
 
             seq_max = find_tcregs(seq_tcr_contact, new_alleles)
@@ -224,9 +233,9 @@ def get_tcreg(hla_file, output_dir, tcreg_file, structure, hla_chain, receptor_c
             for allele, a_list in seq_max.items():
                 seq_length[allele] = len(a_list)
             
-            seq_length = {k: v for k, v in sorted(seq_length.items(), key=lambda item: item[1], reverse=True)} # sort dictionary from largest list to smallest list
+            seq_length = {k: v for k, v in sorted(seq_length.items(), key=lambda item: item[1], reverse=True)}
 
-            # artificial representative - this will be changed to the most common allele in the T-CREG later
+            # Temporary representative - this will be changed to the most common allele in the T-CREG later
             best_rep = list(seq_length.keys())[0]
             temp_tcreg[best_rep] = seq_max[best_rep]
 
@@ -239,6 +248,7 @@ def get_tcreg(hla_file, output_dir, tcreg_file, structure, hla_chain, receptor_c
             # print(f"Taking out {best_rep} and its {len(seq_max)} neighbors leaving {len(new_alleles)} alleles")
             if len(new_alleles) == 0: break
 
+        # Convert temporary T-CREGs to final format with most common allele as representative
         temp_tcreg = dict(sorted(temp_tcreg.items(), key=lambda x:x[0]))
         tcreg = defaultdict(list)
         for temp_allele, allele_list in temp_tcreg.items():
@@ -257,7 +267,7 @@ def get_tcreg(hla_file, output_dir, tcreg_file, structure, hla_chain, receptor_c
             
             tcreg[most_common_allele] = allele_list
         
-        filename = f"{output_dir}/{os.path.basename(structure.split('.pdb')[0])}_tcreg.txt"
+        filename = f"{output_dir}/{Path(structure).stem}_tcreg.txt"
         write_tcreg_file(filename, tcreg, usa_allelic_freq, tcr_contact_resis, seq_tcr_contact)
 
         print(f"A new T-CREG list was generated in {filename}")
@@ -267,6 +277,7 @@ def get_tcreg(hla_file, output_dir, tcreg_file, structure, hla_chain, receptor_c
         binders = txtfile.readlines()
         binders = [allele.strip().replace("HLA", "").replace("-", "") for allele in binders]
     
+    # Extract TCR contact sequences for all HLA alleles
     seq_tcr_contact = defaultdict(str)
     for allele, seq in hla_seq.items():
         str_tcr_contact = ''
@@ -285,8 +296,7 @@ def get_tcreg(hla_file, output_dir, tcreg_file, structure, hla_chain, receptor_c
                 considered_alleles.append(allele)
     
     if len(tcreg_binders) == 0:
-        print(f"No T-CREGs found that ovelap with binders. Is your HLA list ({hla_file}) correct?")
-        quit()
+        raise ValueError(f"No T-CREGs found that overlap with binders. Is your HLA list ({hla_file}) correct?")
     
     with open(f"{output_dir}/tcreg_final.txt", "w") as txtfile:
         for creg, allele_list in tcreg_binders.items():
@@ -295,9 +305,9 @@ def get_tcreg(hla_file, output_dir, tcreg_file, structure, hla_chain, receptor_c
             for allele in allele_list:
                 total_allelic_freq += usa_allelic_freq[allele]
 
-            txtfile.write(f"{creg} T-CREG | {round(total_allelic_freq*100,2)}%\n")
+            txtfile.write(f"{creg} T-CREG | {total_allelic_freq*100:.2f}%\n")
             for allele in allele_list:
-                txtfile.write(f" - {allele} | {round(usa_allelic_freq[allele]*100,2)}%\n")
+                txtfile.write(f" - {allele} | {usa_allelic_freq[allele]*100:.2f}%\n")
     
 if __name__ == "__main__":
 
